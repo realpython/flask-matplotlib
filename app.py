e93d425673056249257fcc1c8eecf85022308c46
Michael Herman <hermanmu@gmail.com>
2015-05-11 08:57:25 -0600

    initial

---
+++ app.py
@@ -0,0 +1,32 @@
+import random
+from io import BytesIO
+# from StringIO import StringIO  # python 2.7x
+
+from flask import Flask, make_response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+
+app = Flask(__name__)
+
+
+@app.route('/plot.png')
+def plot():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+
+    axis.plot(xs, ys)
+    canvas = FigureCanvas(fig)
+    output = BytesIO()
+    # output = StringIO()  # python 2.7x
+    canvas.print_png(output)
+    response = make_response(output.getvalue())
+    response.mimetype = 'image/png'
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
